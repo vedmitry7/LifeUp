930d3f3d4504edbc2b4718742f919d252708d99f
vedmitry7 <vedmitry7@gmail.com>
2018-03-02 22:41:58 +0300

    fix status dialog

--- app/src/main/java/com/vedmitryapps/lifeup/StatusDialog.java
+++ app/src/main/java/com/vedmitryapps/lifeup/StatusDialog.java
@@ -5,7 +5,9 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class StatusDialog extends Dialog {
     public StatusDialog(@NonNull Context context) {
@@ -19,5 +21,13 @@ public class StatusDialog extends Dialog {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_status);
 
+
+        Button setButton = findViewById(R.id.setStatus_btn);
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
     }
 }
